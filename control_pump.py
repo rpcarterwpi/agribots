451707eacb2c93a68e957c30eab75f945b735d92
stingraypcarter <stingraypcarter@gmail.com>
2022-03-23 14:50:53 -0400

    testing pump stuff

---
+++ control_pump.py
@@ -0,0 +1,24 @@
+# import RPi.GPIO as GPIO
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(26, GPIO.OUT)
+#
+
+
+import gpiozero
+
+from time import sleep
+
+led = gpiozero.LED(26)
+for i in range(10):
+    print('high')
+    led.on()
+    time.sleep(0.2)
+    led.off()
+    print('low')
+    time.sleep(0.2)
+
+# led = PWMLED(2)
+#
+# for b in range(101):
+#     led.value = b / 100.0
+#     sleep(0.01)
