3d3d66f5d55834e18734b0c0edcbbe5fb9447d77
stingraypcarter <stingraypcarter@gmail.com>
2022-03-29 13:41:00 -0400

    other motor controllers

---
+++ pwm160d.py
@@ -0,0 +1,14 @@
+# Imported Libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+# SetMode
+GPIO.setmode(GPIO.BOARD)
+
+# Motor 1 Setup
+PWR1, ENA1, IN1, IN2, GND = 2, 33, 31, 29, 39
+GPIO.setup(ENA1, GPIO.OUT)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+PWMA = GPIO.PWM(ENA1, 50)
+PWMA.start(0)
