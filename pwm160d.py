2f930122140877d036f0489ef2247a1e7975b6bb
stingraypcarter <stingraypcarter@gmail.com>
2022-03-30 14:15:02 -0400

    Update pwm160d.py

--- pwm160d.py
+++ pwm160d.py
@@ -10,16 +10,29 @@ ENA1, IN1, IN2 = 33, 29, 31
 GPIO.setup(ENA1, GPIO.OUT)
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
+
+ENA2, IN3, IN4 = 32, 16, 18
+GPIO.setup(ENA2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+
+
 #
 PWMA = GPIO.PWM(ENA1, 100)
 PWMA.start(60)
+PWMA = GPIO.PWM(ENA2, 100)
+PWMA.start(60)
+
 # GPIO.output(IN1, GPIO.LOW)
 # GPIO.output(ENA1, GPIO.LOW)
 # GPIO.output(IN2, GPIO.LOW)
+# GPIO.output(ENA1, GPIO.HIGH)
+
 
 GPIO.output(IN1, GPIO.HIGH)
-# GPIO.output(ENA1, GPIO.HIGH)
 GPIO.output(IN2, GPIO.LOW)
+GPIO.output(IN3, GPIO.HIGH)
+GPIO.output(IN4, GPIO.LOW)
 #
 sleep(5)
 GPIO.cleanup()
