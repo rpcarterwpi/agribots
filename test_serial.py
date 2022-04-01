0e8b4b2d8d074b34cb81bef13f5d67b895e05df1
stingraypcarter <stingraypcarter@gmail.com>
2022-04-01 15:56:13 -0400

    serial testing

---
+++ test_serial.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os, sys
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyUSB0',19200, timeout = 5)
+
+# listen for the input, exit if nothing received in timeout period
+while True:
+    print(ser.readline())
