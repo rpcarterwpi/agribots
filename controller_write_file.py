745326609ec261abe16a1d27087c2140120a5b85
stingraypcarter <stingraypcarter@gmail.com>
2022-04-05 16:41:34 -0400

    Update controller_write_file.py

--- controller_write_file.py
+++ controller_write_file.py
@@ -19,10 +19,10 @@ cur_drivemode_R = DriveMode.COAST
 
 def write_vals():
     f = open('actions.txt', 'w')
-    print(cur_effort_L)
-    print(cur_effort_R)
-    print(cur_drivemode_L)
-    print(cur_drivemode_L)
+    # print(cur_effort_L)
+    # print(cur_effort_R)
+    # print(cur_drivemode_L)
+    # print(cur_drivemode_L)
     f.write(str(cur_effort_L)+'\n')
     f.write(str(cur_effort_R)+'\n')
     f.write(str(int(cur_drivemode_L))+'\n')
@@ -54,48 +54,54 @@ class MyController(Controller):
 
     def on_L3_up(self,value):
         effort = MAX_SPEED*normalize_joystick(True,value)
-        print(f'L Forward: {effort}')
+        # print(f'L Forward: {effort}')
         # tank_drive(DriveMode.DRIVE,effort,Motors.LEFT)
         cur_drivemode_L = DriveMode.DRIVE
         cur_effort_L = effort
+        print(cur_efort_L)
         write_vals()
 
     def on_L3_down(self,value):
         effort = -1*MAX_SPEED*normalize_joystick(False,value)
-        print(f'L Back: {effort}')
+        # print(f'L Back: {effort}')
         # tank_drive(DriveMode.DRIVE,effort,Motors.LEFT)
         cur_drivemode_L = DriveMode.DRIVE
         cur_effort_L = effort
+        print(cur_efort_L)
         write_vals()
 
     def on_L3_y_at_rest(self):
-        print('L Coast')
+        # print('L Coast')
         # tank_drive(DriveMode.COAST,0,Motors.LEFT)
         cur_drivemode_L = DriveMode.COAST
         cur_effort_L = 0
+        print(cur_efort_L)
         write_vals()
 
     def on_R3_up(self,value):
         effort = MAX_SPEED*normalize_joystick(True,value)
-        print(f'R Forward: {effort}')
+        # print(f'R Forward: {effort}')
         # tank_drive(DriveMode.DRIVE,effort,Motors.RIGHT)
         cur_drivemode_R = DriveMode.DRIVE
         cur_effort_R = effort
+        print(cur_efort_R)
         write_vals()
 
     def on_R3_down(self,value):
         effort = -1*MAX_SPEED*normalize_joystick(False,value)
-        print(f'R Back: {effort}')
+        # print(f'R Back: {effort}')
         # tank_drive(DriveMode.DRIVE,effort,Motors.RIGHT)
         cur_drivemode_R = DriveMode.DRIVE
         cur_effort_R = effort
+        print(cur_efort_R)
         write_vals()
 
     def on_R3_y_at_rest(self):
-        print('R Coast')
+        # print('R Coast')
         # tank_drive(DriveMode.COAST,0,Motors.RIGHT)
         cur_drivemode_R = DriveMode.COAST
         cur_effort_R = 0
+        print(cur_efort_R)
         write_vals()
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=True)
