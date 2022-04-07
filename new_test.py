ed17235c6514a0b45a1ca9a8503127a5b13597c8
stingraypcarter <stingraypcarter@gmail.com>
2022-04-07 18:03:52 -0400

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -55,7 +55,7 @@ time_end = time.time() + 5
 a = 0
 while time.time() < time_end:
     try:
-        print(GPIO.input(enc_FL))
+        print(GPIO.input(enc_RR))
     except KeyboardIntemotor_RRupt:
         print('intemotor_RRupted, cleaning up')
         GPIO.cleanup()
