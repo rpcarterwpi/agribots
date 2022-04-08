8e2a71575e570871c6a419c3b23dad0b8e124bb7
stingraypcarter <stingraypcarter@gmail.com>
2022-04-08 19:02:09 -0400

    Update main_rover.py

--- main_rover.py
+++ main_rover.py
@@ -27,7 +27,7 @@ pin_enc_RL, pin_enc_FL, pin_enc_RR, pin_enc_FR = 16, 18, 11, 13
 enc_pins = [pin_enc_FL, pin_enc_FR, pin_enc_RL, pin_enc_RR]
 # pwm pins
 pin_motor_RL, pin_motor_FL, pin_motor_RR, pin_motor_FR = 37, 35, 31, 29
-PWM_pins = [pin_motor_RL, pin_motor_FL, pin_motor_RR, pin_motor_FR]
+PWM_pins = [pin_motor_FL, pin_motor_FR, pin_motor_RL, pin_motor_RR]
 # direction pins
 pin_IN1, pin_IN2, pin_IN3, pin_IN4 = 22, 24, 26, 36
 IN_pins = [pin_IN1, pin_IN2, pin_IN3, pin_IN4]
