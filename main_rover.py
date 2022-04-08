a6c3d775354c2e10fb5dfcf5569cc456df670ea1
stingraypcarter <stingraypcarter@gmail.com>
2022-04-08 17:33:39 -0400

    Update main_rover.py

--- main_rover.py
+++ main_rover.py
@@ -78,7 +78,7 @@ def motors_write_raw(motors_write):
 # repeated actions
 def encoder_actions():
     global enc_vel, enc_pos_data, enc_history, pose, pose_ee, turn_ee, pose_ee_t, tick_count
-    enc_vel, enc_pos_data, enc_history, tick_count = enc.encoder_measure(encoder_read_raw(),enc_pos_data,enc_history)
+    enc_vel, enc_pos_data, enc_history, tick_count = enc.encoder_measure(encoder_read_raw(),enc_pos_data,enc_history, tick_count)
     # TEMPORARY
     # pose_ee, turn_ee, pose_ee_t = enc.encoder_estimate(enc_vel, pose, pose_ee_t)
 
