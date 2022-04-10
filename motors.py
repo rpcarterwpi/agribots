39402a80caef312b06e5660bb78c1cbd403bffdc
stingraypcarter <stingraypcarter@gmail.com>
2022-04-10 12:00:23 -0400

    Update motors.py

--- motors.py
+++ motors.py
@@ -9,7 +9,7 @@ class DriveMode(IntEnum):
     COAST = 3
 
 # pid_consts = np.array([300,2,2])
-pid_consts = np.array([2,0,1]) #0.01
+pid_consts = np.array([4,0,2]) #0.01
 int_limit = 100
 
 IN_forward = np.array([1,0])
