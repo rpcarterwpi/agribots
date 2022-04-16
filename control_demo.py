e23ef7c2c61c3c3bbac89ae3476b4216bc2def6c
stingraypcarter <stingraypcarter@gmail.com>
2022-04-16 15:57:50 -0400

    Update control_demo.py

--- control_demo.py
+++ control_demo.py
@@ -8,6 +8,31 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+
+    def on_L3_up(self, value):
+        value = value/-32767
+        print(f'left: {value}')
+
+    def on_L3_down(self, value):
+        value = value/-32767
+        print(f'left: {value}')
+
+    def on_L3_x_at_rest(self):
+        value = 0
+        print(f'left: {value}')
+
+    def on_R3_up(self, value):
+        value = value/-32767
+        print(f'right: {value}')
+
+    def on_R3_down(self, value):
+        value = value/-32767
+        print(f'right: {value}')
+
+    def on_R3_x_at_rest(self):
+        value = 0
+        print(f'right: {value}')
+
     def on_L3_left(self, value):
         a = 0
 
@@ -18,6 +43,12 @@ class MyController(Controller):
         a = 0
 
     def on_R3_right(self, value):
+        a = 0
+
+    def on_L3_x_at_rest(self):
+        a = 0
+
+    def on_R3_x_at_rest(self):
         a = 0
 
 
