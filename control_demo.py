7bf9f1e90601472a6fed87f9da4fb45ef8ba156a
stingraypcarter <stingraypcarter@gmail.com>
2022-04-16 15:54:08 -0400

    Update control_demo.py

--- control_demo.py
+++ control_demo.py
@@ -8,7 +8,17 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    # def
+    def on_L3_left(self, value):
+        a = 0
+
+    def on_L3_right(self, value):
+        a = 0
+
+    def on_R3_left(self, value):
+        a = 0
+
+    def on_R3_right(self, value):
+        a = 0
 
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
